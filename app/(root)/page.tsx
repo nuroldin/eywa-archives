af30e76a07777299de707cd6f2c25d2ccf06b7bb
nuroldin <nuroldin.umpara.pimping08@gmail.com>
2025-06-09 17:07:37 +0800

    New Updates

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
+import { sampleBooks } from "@/constants";
 
 const Home = () => (
 	<>
-		<BookOverview />
-		<BookList />
+		<BookOverview {...sampleBooks[0]} />
+		<BookList
+			title="Latest Books"
+			books={sampleBooks}
+			containerClassName="mt-28"
+		/>
 	</>
 );
 

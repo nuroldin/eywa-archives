82b7614597d47411ff6b64d22d84ca4c2b8d8c69
nuroldin <nuroldin.umpara.pimping08@gmail.com>
2025-06-13 17:12:20 +0800

    feat: add ImageKit integration for image upload functionality

--- components/ImageUpload.tsx
+++ components/ImageUpload.tsx
@@ -1,7 +1,46 @@
-import React from "react";
+"use client";
+import config from "@/lib/config";
+import ImageKit from "imagekit";
+import { Image, ImageKitProvider } from "@imagekit/next";
+import React, { useRef, useState } from "react";
+
+const {
+	env: {
+		imagekit: { publicKey, urlEndpoint },
+	},
+} = config;
+
+const authenticator = async () => {
+	try {
+		const response = await fetch(`${config.env.apiEndpoint}/api/auth/imagekit`);
+
+		if (!response.ok) {
+			const errorText = await response.text();
+
+			throw new Error(
+				`Request failed with status ${response.status}: ${errorText}`
+			);
+		}
+
+		const data = await response.json();
+
+		const { signature, expire, token } = data;
+
+		return { token, expire, signature };
+	} catch (error: any) {
+		throw new Error(`Authentication request failed: ${error.message}`);
+	}
+};
 
 const ImageUpload = () => {
-	return <div>Image Upload</div>;
+	const ikUploadRef = useRef(null);
+	const [file, setFile] = useState<{ filePath: string } | null>(null);
+
+	return (
+		<ImageKitProvider urlEndpoint={urlEndpoint}>
+			<IKUpload />
+		</ImageKitProvider>
+	);
 };
 
 export default ImageUpload;
